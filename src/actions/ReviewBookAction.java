58d62d76127ef3d64e7e9f3e40db3ebf9a66217f
Gill Rowan <C10327317@mydit.ie>
2014-04-13 20:56:32 +0100

    Added notion of review

--- src/actions/ReviewBookAction.java
+++ src/actions/ReviewBookAction.java
@@ -10,5 +10,28 @@ public class ReviewBookAction extends ActionSupport {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private boolean reviewMe;
+
+	public boolean isReviewMe() {
+		return reviewMe;
+	}
+
+	public void setReviewMe(boolean reviewMe) {
+		this.reviewMe = reviewMe;
+	}
+	
+	public String execute(){
+		if(reviewMe){
+			return SUCCESS;
+		}
+		else{
+			return "failure";
+		}
+	}
+	
+	public String submit(){
+		return SUCCESS;
+	}
 
 }
